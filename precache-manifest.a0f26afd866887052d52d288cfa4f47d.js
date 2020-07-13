7be70cc39288d7f24e5d668845d5254fb6e3cda3
Guilherme de Almeida Costa <costaguila@gmail.com>
2020-07-13 16:18:15 -0300

    Atualizando projeto

---
+++ precache-manifest.a0f26afd866887052d52d288cfa4f47d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebeedd4f95dcd61ebbf3cddcfb263f38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "565e5b0459636ab5d848",
+    "url": "/static/css/2.78668cc7.chunk.css"
+  },
+  {
+    "revision": "05a87833178f0572a53c",
+    "url": "/static/css/main.6a6182d6.chunk.css"
+  },
+  {
+    "revision": "565e5b0459636ab5d848",
+    "url": "/static/js/2.76142057.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.76142057.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05a87833178f0572a53c",
+    "url": "/static/js/main.c221f15b.chunk.js"
+  },
+  {
+    "revision": "890ce1e363f0200a5cec",
+    "url": "/static/js/runtime-main.5d3c694c.js"
+  }
+]);
