4866d0c6681ecb8dac4367ac20154d197858e59d
Guilherme de Almeida Costa <costaguila@gmail.com>
2020-07-13 15:53:38 -0300

    Inicio totem profit

---
+++ precache-manifest.10f13d21b75f7291e489d4b3d615e68c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e307301bb4e2ecd6443b4059bb24d4c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "565e5b0459636ab5d848",
+    "url": "/static/css/2.78668cc7.chunk.css"
+  },
+  {
+    "revision": "7ec40198b866071cfcef",
+    "url": "/static/css/main.539323d1.chunk.css"
+  },
+  {
+    "revision": "565e5b0459636ab5d848",
+    "url": "/static/js/2.76142057.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.76142057.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ec40198b866071cfcef",
+    "url": "/static/js/main.fc0f46c5.chunk.js"
+  },
+  {
+    "revision": "890ce1e363f0200a5cec",
+    "url": "/static/js/runtime-main.5d3c694c.js"
+  }
+]);
